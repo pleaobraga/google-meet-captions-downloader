4bd61dd7917fd598690f2de73494cb25056189f9
pleaobraga <pleao.braga@gmail.com>
2025-08-25 13:24:33 -0300

    feat: update buttons layout

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -52,30 +52,34 @@ function App() {
     <>
       <div className="flex flex-col justify-center items-center gap-5 p-3">
         <h2 className="text-xl">Transcribing your Meeting</h2>
-        <div className="flex gap-3">
-          <button
-            className={`bg-blue-500 text-white py-2 px-2 rounded ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
-            onClick={getCaptionsTranscript}
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <PulseLoader color="white" size={12} />
-            ) : (
-              'Get Transcription'
-            )}
-          </button>
-          <button
-            className={`bg-blue-500 text-white py-2 px-3 rounded`}
-            onClick={getFullVideoScreenStyles}
-          >
-            Get Full Screen Video Styles
-          </button>
-          <button
-            className={`bg-blue-500 text-white py-2 px-3 rounded`}
-            onClick={getFullVideoScreen}
-          >
-            Apply Full Screen Video
-          </button>
+        <div className="flex flex-col gap-3">
+          <div>
+            <button
+              className={`bg-blue-500 text-white py-2 px-2 rounded ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+              onClick={getCaptionsTranscript}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <PulseLoader color="white" size={12} />
+              ) : (
+                'Get Transcription'
+              )}
+            </button>
+          </div>
+          <div>
+            <button
+              className={`bg-blue-500 text-white py-2 px-3 rounded`}
+              onClick={getFullVideoScreenStyles}
+            >
+              Get Full Screen Video Styles
+            </button>
+            <button
+              className={`bg-blue-500 text-white py-2 px-3 rounded`}
+              onClick={getFullVideoScreen}
+            >
+              Apply Full Screen Video
+            </button>
+          </div>
         </div>
       </div>
       <Toaster position="top-right" duration={2000} />
