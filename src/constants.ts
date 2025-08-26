9fb7377f085752442ccf76b3af828c28c50199d5
pleaobraga <pleao.braga@gmail.com>
2025-08-26 10:26:39 -0300

    feat: create consts

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const PORPORTION_WIDTH_RATE = 1.777
+export const HEIGHT_AROUND_VIDEO = 96
+export const WIDTH_PADDING = 32
+
+export const MAX_VIDEO_HEIGHT = window.innerHeight - HEIGHT_AROUND_VIDEO
+export const MAX_VIDEO_WIDTH = MAX_VIDEO_HEIGHT * PORPORTION_WIDTH_RATE
