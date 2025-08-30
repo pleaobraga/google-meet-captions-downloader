8756f0beb35d560ae097be71419b8b8f41189eda
pleaobraga <pleao.braga@gmail.com>
2025-08-30 08:40:28 -0300

    feat: save caption on local storage

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,17 +1,52 @@
-const observer = new MutationObserver(() => {
-  const caption = document.querySelector<HTMLButtonElement>(
-    '[aria-label="Turn on captions"]'
-  )
+let hasCaptionObserver = false
 
-  if (caption) {
-    // open captions
-    caption.click()
-  }
-})
+let captionText: HTMLDivElement | null = null
 
 const body = document.querySelector('body')
 
-observer.observe(body!, {
-  childList: true,
-  subtree: true,
+const currentStorageItem = `captionText_${Date.now()}`
+
+console.log('currentStorageItem', currentStorageItem)
+
+const observerCaptions = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (captionText && mutation.type === 'characterData') {
+      saveCaptionText(captionText.innerText)
+    }
+  })
 })
+
+const observerBody = new MutationObserver(() => {
+  const captionToogle = document.querySelector<HTMLButtonElement>(
+    '[aria-label="Turn on captions"]'
+  )
+  captionText = document.querySelector<HTMLDivElement>(
+    '[aria-label="Captions"]'
+  )
+
+  if (captionToogle) {
+    // open captions
+    captionToogle.click()
+  }
+
+  if (captionText && !hasCaptionObserver) {
+    hasCaptionObserver = true
+    observerCaptions.observe(captionText, {
+      childList: true,
+      subtree: true,
+      characterData: true,
+    })
+  }
+})
+
+if (body) {
+  observerBody.observe(body, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  })
+}
+
+function saveCaptionText(captionText: string) {
+  localStorage.setItem(currentStorageItem, captionText)
+}
